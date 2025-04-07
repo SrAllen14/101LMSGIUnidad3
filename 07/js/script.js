a9c3ab2a57da1b25fa71504ab55e3a5dbe1e7302
Alvaro AllPer <alvaro.allper.1@educa.jcyl.es>
2025-04-07 08:37:58 +0200

    Principio ejercicio 7

--- 07/js/script.js
+++ 07/js/script.js
@@ -1,41 +1,19 @@
-document.getElementById("agregarFinal").addEventListener("click", function(){
-    // Crear un nuevo párrafo
-   
-    //añadir texto al párrafo
-  
-    //añadir el párrafo al contenedor
-   
-    //agregar el color de texto del nuevo parrafo sea rojo
-    parrafo.style.color = "red";
-});
-//agregar párrafo al principio
-document.getElementById("agregarPpio").addEventListener("click", function(){
-    // Crear un nuevo párrafo
-   
-    //añadir texto al párrafo
-   
-    //añadir el párrafo al contenedor .prepend(parrafo)
-   
-    //agregar estilos al párrafo texto azul
-   
-});
-//agregar párrafo en una posición concreta
-document.getElementById("agregarPosicion").addEventListener("click", function(){
-    // Crear un nuevo párrafo
-  
-    //añadir texto al párrafo
-  
-    //añadir el párrafo al contenedor inserBefore
-  
-    //agregar estilos color del texto verde
+function agregarFinal(){
+    var cuadroTexto = document.getElementById("texto").value;
+    var caja = document.getElementById("contenedor");
+
+    if (cuadroTexto.trim() != ""){
+        var parrafo = document.createElement("p");
+        parrafo.textContent=cuadroTexto;
+        caja.appendChild(parrafo);
+    }
+    else {
+        alert("Cuadro de texto vacio.");
+    }
     
-});
-//eliminar el primer párrafo
-document.getElementById("eliminar").addEventListener("click", function(){
-    //eliminar el primer párrafo
-   
-});
-document.getElementById("eliminarTodos").addEventListener("click", function(){
-    //eliminar todos los párrafos
-   
-});
+}
+
+/*
+    Usa classList.toggle agrega o quita la clase automaticamente.
+    parrafos[i].classList.toggle("estilo-modificado");
+*/ 
